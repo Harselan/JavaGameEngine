fcbd69c4545e6083593f1a095e7ff79f1580cdd3
Harselan <haeressel@gmail.com>
2018-07-09 12:36:53 +0200

    Har lagt till inputs för tangentbordet och musen

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -1,5 +1,7 @@
 package com.base.engine;
 
+import org.lwjgl.input.Keyboard;
+
 public class Game 
 {
 	public Game()
@@ -9,7 +11,25 @@ public class Game
 	
 	public void input()
 	{
+		if( Input.getKeyDown(Keyboard.KEY_UP) )
+		{
+			System.out.println("We have just pressed up!");
+		}
 		
+		if( Input.getKeyUp(Keyboard.KEY_UP) )
+		{
+			System.out.println("We have just released up!");
+		}
+		
+		if( Input.getMouseDown(1) )
+		{
+			System.out.println("We have just rightclicked at " + Input.getMousePosition().toString() + "!");
+		}
+		
+		if( Input.getMouseUp(1) )
+		{
+			System.out.println("We have just released rightclick!");
+		}
 	}
 	
 	public void update()
--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -77,6 +77,7 @@ public class MainComponent
 				}
 				
 				Time.setDelta(frameTime);
+				Input.update();
 				
 				game.input();
 				game.update();
