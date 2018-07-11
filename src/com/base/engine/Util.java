ce7dc3a09cef8e5f4ae424f0c3781ffe3c4e8e2a
Harselan <haeressel@gmail.com>
2018-07-11 16:33:25 +0200

    Har ändrat Mesh systemet så att mer kod är återanvändbar. Gjorde även en 3d pyramid

--- src/com/base/engine/Util.java
+++ src/com/base/engine/Util.java
@@ -1,6 +1,7 @@
 package com.base.engine;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import org.lwjgl.BufferUtils;
 
@@ -11,7 +12,22 @@ public class Util
 		return BufferUtils.createFloatBuffer(size);
 	}
 	
-	public static FloatBuffer createFilippedBuffer( Vertex[] vertices )
+	public static IntBuffer createIntBuffer( int size )
+	{
+		return BufferUtils.createIntBuffer(size);
+	}
+	
+	public static IntBuffer createFlippedBuffer( int... values )
+	{
+		IntBuffer buffer = createIntBuffer( values.length );
+		
+		buffer.put( values );
+		buffer.flip();
+	
+		return buffer;
+	}
+	
+	public static FloatBuffer createFlippedBuffer( Vertex[] vertices )
 	{
 		FloatBuffer buffer = createFloatBuffer( vertices.length * Vertex.SIZE );
 		
