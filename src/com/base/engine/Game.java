2ded1c7bb2c9bd847ba420298b55d42688278056
Harselan <haeressel@gmail.com>
2018-07-11 10:31:10 +0200

    Har gjort så att objekt kan röra sig på bilden i både x,y och z axeln med hjälp av transform klassen

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -6,6 +6,7 @@ public class Game
 {
 	private Mesh mesh;
 	private Shader shader;
+	private Transform transform;
 	
 	public Game()
 	{
@@ -20,12 +21,14 @@ public class Game
 		
 		mesh.addVertices(data);
 		
+		transform = new Transform();
+		
 		shader.addVertexShader( ResourceLoader.loadShader( "basicVertex.vs" ) );
 		shader.addFragmentShader( ResourceLoader.loadShader( "basicFragment.fs" ) );
 		
 		shader.compileShader();
 		
-		shader.addUniform("uniformFloat");
+		shader.addUniform("transform");
 	}
 	
 	public void input()
@@ -57,12 +60,13 @@ public class Game
 	{
 		temp += Time.getDelta();
 		
-		shader.setUniformf( "uniformFloat", (float)Math.abs( Math.sin(temp) ) );
+		transform.setTranslation( (float)Math.sin(temp), 0, 0 );
 	}
 	
 	public void render()
 	{
 		shader.bind();
+		shader.setUniform( "transform", transform.getTransformation() );
 		mesh.draw();
 	}
 }
