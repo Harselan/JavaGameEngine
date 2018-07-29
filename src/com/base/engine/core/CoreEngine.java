72912abfcae3ff87ff39c990d028ecab0235881d
Harselan <haeressel@gmail.com>
2018-07-29 21:44:49 +0200

    Har uppdaterat strukturen för spelmotorn

---
+++ src/com/base/engine/core/CoreEngine.java
@@ -0,0 +1,144 @@
+package com.base.engine.core;
+
+import com.base.engine.rendering.RenderUtil;
+import com.base.engine.rendering.Window;
+import com.base.game.Game;
+
+public class CoreEngine 
+{	
+	private boolean isRunning;
+	private Game game;
+	private int width;
+	private int height;
+	private double frameTime;
+	
+	public CoreEngine( int width, int height, double framerate, Game game )
+	{
+		this.isRunning = false;
+		this.game = game;
+		this.width = width;
+		this.height = height;
+		this.frameTime = 1.0 / framerate;
+	}
+	
+	private void initializeRenderingSystem()
+	{
+		System.out.println(RenderUtil.getOpenGlVersion());
+		RenderUtil.initGraphics();
+	}
+	
+	public void createWindow( String title )
+	{
+		Window.createWindow( width, height, title );
+		initializeRenderingSystem();
+	}
+	
+	public void start()
+	{
+		if( isRunning )
+		{
+			return;
+		}
+		
+		run();
+	}
+	
+	public void stop()
+	{
+		if( !isRunning )
+		{
+			return;
+		}
+		
+		isRunning = false;
+	}
+	
+	private void run()
+	{
+		// Säger till att spelet körs nu
+		isRunning = true;
+		
+		// Fixar till fps
+		int frames = 0;
+		long frameCounter = 0;
+		
+		game.init();
+		
+		// Tidpunken allting börjar
+		long lastTime = Time.getTime();
+		// Tid mellan varje renderad bild
+		double unprocessedTime = 0;
+		
+		while( isRunning )
+		{
+			boolean render = false;
+			
+			long startTime = Time.getTime();
+			long passedTime = startTime - lastTime;
+			lastTime = startTime;
+			
+			// Lägger till tiden som har gått i sekunder till unprocessedTime
+			unprocessedTime  += passedTime / (double)Time.SECOND;
+			frameCounter += passedTime;
+			
+			// När det är dags för en ny renderad bild
+			while( unprocessedTime > frameTime )
+			{
+				render = true;
+				unprocessedTime -= frameTime;
+			
+				if( Window.isCloseRequested() )
+				{
+					stop();
+				}
+				
+				Time.setDelta(frameTime);
+				
+				game.input();
+				Input.update();
+				game.update();
+				
+				// När det har gått en sekund
+				if( frameCounter >= Time.SECOND )
+				{
+					System.out.println(frames);
+					frames = 0;
+					frameCounter = 0;
+				}
+			}
+			
+			// Kollar om en ny bild behöver renderas in
+			if( render )
+			{
+				render();	
+				frames++;
+			}
+			else
+			{
+				try 
+				{
+					Thread.sleep(1);
+				} 
+				catch( InterruptedException e ) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		cleanUp();
+	}
+	
+	private void render()
+	{
+		RenderUtil.clearScreen();
+		game.render();
+		Window.render();
+	}
+	
+	private void cleanUp()
+	{
+		Window.dispose();
+	}
+}
---
+++ src/com/base/engine/rendering/RenderUtil.java
@@ -0,0 +1,64 @@
+package com.base.engine.rendering;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL32.GL_DEPTH_CLAMP;
+
+import com.base.engine.core.Vector3f;
+
+public class RenderUtil 
+{
+	public static void clearScreen()
+	{
+		//TODO: Stencil Buffer
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	}
+	
+	public static void setTextures( boolean enabled )
+	{
+		if( enabled )
+		{
+			glEnable( GL_TEXTURE_2D );
+		}
+		else
+		{
+			glDisable( GL_TEXTURE_2D );
+		}
+	}
+	
+	public static void unbindTextures()
+	{
+		glBindTexture( GL_TEXTURE_2D, 0 );
+	}
+	
+	public static void setClearColor( Vector3f color )
+	{
+		glClearColor( color.getX(), color.getY(), color.getZ(), 1.0f );
+	}
+	
+	public static void initGraphics()
+	{
+		// Sätter så att skärmen rensas till en svart färg
+		glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+		
+		// Säger att den främre delen bestäms av GL_CW vilket då är i klockans håll
+		glFrontFace( GL_CW );
+		// Gör så att den bakre renderingen inte skapas för den visas inte för kameran
+		glCullFace( GL_BACK );
+		glEnable( GL_CULL_FACE );
+		// Får programmet att hålla koll på hur långt alla renderingar är från kameran 
+		glEnable( GL_DEPTH_TEST );
+		
+		//TODO: Depth clamp for later
+		glEnable( GL_DEPTH_CLAMP );
+		// Fixar så att texturer används
+		glEnable( GL_TEXTURE_2D );
+		// Fixar så att färgerna inte blir allt för mörka
+		//glEnable( GL_FRAMEBUFFER_SRGB );
+	}
+	
+	public static String getOpenGlVersion()
+	{
+		return glGetString( GL_VERSION );
+	}
+}
