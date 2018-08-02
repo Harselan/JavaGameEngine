140bcc88bbe252fb1cd443ecd02c4298df3edc47
Harselan <haeressel@gmail.com>
2018-08-02 20:26:03 +0200

    Har börjat lägga till ljus på renderingsmotorn

---
+++ src/com/base/engine/rendering/ForwardAmbient.java
@@ -0,0 +1,38 @@
+package com.base.engine.rendering;
+
+import com.base.engine.core.Matrix4f;
+import com.base.engine.core.Transform;
+
+public class ForwardAmbient extends Shader
+{
+private static final ForwardAmbient instance = new ForwardAmbient();
+	
+	public static ForwardAmbient getInstance()
+	{
+		return instance;
+	}
+	
+	private ForwardAmbient()
+	{	
+		addVertexShaderFromFile( "forwardAmbient.vs" );
+		addFragmentShaderFromFile( "forwardAmbient.fs" );
+		
+		setAttribLocation( "position", 0 );
+		setAttribLocation( "texCoord", 1 );
+		
+		compileShader();
+		
+		addUniform( "MVP" );
+		addUniform( "ambientIntensity" );
+	}
+	
+	public void updateUniforms( Transform transform, Material material )
+	{
+		Matrix4f worldMatrix = transform.getTransformation();
+		Matrix4f projectedMatrix = getRenderingEngine().getMainCamera().getViewProjection().mul(worldMatrix);
+		material.getTexture().bind();
+		
+		setUniform( "MVP", projectedMatrix );
+		setUniform( "ambientIntensity", getRenderingEngine().getAmbientLight() );
+	}
+}
